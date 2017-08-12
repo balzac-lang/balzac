97fb1da6ea80fb8b81878c091dd8f3b77229d414
Nicola Atzei <atzeinicola@gmail.com>
2017-08-12 21:32:09 +0200

    .hgignore

---
+++ dsl/it.unica.tcs.bitcointm.parent/it.unica.tcs.bitcointm.lib/src/main/java/it/unica/tcs/bitcointm/lib/CoinbaseTransactionBuilder.java
@@ -0,0 +1,19 @@
+package it.unica.tcs.bitcointm.lib;
+
+public class CoinbaseTransactionBuilder extends TransactionBuilder {
+
+	@Override
+	public TransactionBuilder addInput(ScriptBuilder2 inputScript) {
+		return super.addInput(inputScript);
+	}
+
+	@Override
+	public TransactionBuilder addInput(ITransactionBuilder tx, int outIndex, ScriptBuilder2 inputScript) {
+		throw new UnsupportedOperationException();	
+	}
+
+	@Override
+	public TransactionBuilder addInput(ITransactionBuilder tx, int outIndex, ScriptBuilder2 inputScript, long locktime) {
+		throw new UnsupportedOperationException();	
+	}
+}
