5a766badc16fbcde9ee102d36ab17d4bcbd14f21
Nicola Atzei <atzeinicola@gmail.com>
2017-09-25 09:29:49 +0200

    removed dsl project

--- dsl/it.unica.tcs.bitcointm.parent/it.unica.tcs.bitcointm.lib/src/main/java/it/unica/tcs/bitcointm/lib/prefix/Prefix.java
+++
@@ -1,7 +0,0 @@
-package it.unica.tcs.bitcointm.lib.prefix;
-
-public interface Prefix {
-
-	public boolean ready();
-	public void execute();
-}
--- dsl/it.unica.tcs.bitcointm.parent/it.unica.tcs.bitcointm.lib/src/main/java/it/unica/tcs/bitcointm/lib/process/Process.java
+++
@@ -1,6 +0,0 @@
-package it.unica.tcs.bitcointm.lib.process;
-
-@FunctionalInterface
-public interface Process extends Runnable {
-
-}
