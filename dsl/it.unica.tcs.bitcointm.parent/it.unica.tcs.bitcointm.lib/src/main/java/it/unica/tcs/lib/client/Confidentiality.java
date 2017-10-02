7dd5c51062873cf65511949fcc4fb064efa9f04e
Nicola Atzei <atzeinicola@gmail.com>
2017-10-02 17:34:04 +0200

    dsl: lib: Reliability -> Confidentility

---
+++ dsl/it.unica.tcs.bitcointm.parent/it.unica.tcs.bitcointm.lib/src/main/java/it/unica/tcs/lib/client/Confidentiality.java
@@ -0,0 +1,18 @@
+package it.unica.tcs.lib.client;
+
+public enum Confidentiality {
+
+	LOW(1),
+	MEDIUM(3),
+	HIGH(6);
+	
+	private final int confirmations;
+	
+	private Confidentiality(int confirmations) {
+		this.confirmations = confirmations;
+	}
+
+	public int getConfirmations() {
+		return confirmations;
+	}
+}
