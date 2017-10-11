8e32a9fde7f3d29b20447e124fb522510ca79cda
Nicola Atzei <atzeinicola@gmail.com>
2017-10-11 18:39:13 +0200

    dsl: fix invalid terminal

--- dsl/it.unica.tcs.bitcointm.examples/src/main/java/com/example/Main.java
+++ dsl/it.unica.tcs.bitcointm.examples/src/main/java/com/example/Main.java
@@ -2,17 +2,17 @@ package com.example;
 
 import org.bitcoinj.core.Transaction;
 
-import com.example.factory.TransactionFactory;
+//import com.example.factory.TransactionFactory;
 
 public class Main {
 
 	
 	public static void main(String[] args) {
 		
-		Transaction T = TransactionFactory.tx_T(Boolean.TRUE).toTransaction();
-		Transaction T1 = TransactionFactory.tx_T1().toTransaction();
-		
-		System.out.println(T.getHashAsString());
-		System.out.println(T1.getHashAsString());
+//		Transaction T = TransactionFactory.tx_T(Boolean.TRUE).toTransaction();
+//		Transaction T1 = TransactionFactory.tx_T1().toTransaction();
+//		
+//		System.out.println(T.getHashAsString());
+//		System.out.println(T1.getHashAsString());
 	}
 }
