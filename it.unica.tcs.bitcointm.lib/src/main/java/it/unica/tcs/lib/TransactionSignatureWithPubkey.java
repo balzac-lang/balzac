76e6942cad4d7a69be9b15626fdb7f4d2ada7167
Nicola Atzei <atzeinicola@gmail.com>
2017-12-14 15:33:27 +0100

    Created TransactionSignatureWithPubkey

---
+++ it.unica.tcs.bitcointm.lib/src/main/java/it/unica/tcs/lib/TransactionSignatureWithPubkey.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2017 Nicola Atzei
+ */
+
+package it.unica.tcs.lib;
+
+import org.bitcoinj.crypto.TransactionSignature;
+
+public class TransactionSignatureWithPubkey extends TransactionSignature {
+
+	private final byte[] pubkey;
+	
+	public TransactionSignatureWithPubkey(TransactionSignature sig, byte[] pubkey) {
+		super(sig.r, sig.s);
+		this.pubkey = pubkey;
+	}
+
+	public byte[] getPubkey() {
+		return pubkey;
+	}
+}
