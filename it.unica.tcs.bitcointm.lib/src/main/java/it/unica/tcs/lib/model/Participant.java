32747d02b66b9135ec39d15dae1f6c807a1eceea
Nicola Atzei <atzeinicola@gmail.com>
2018-01-09 17:47:33 +0100

    Lib: revised socket daemon/client

--- it.unica.tcs.bitcointm.lib/src/main/java/it/unica/tcs/lib/model/Participant.java
+++ it.unica.tcs.bitcointm.lib/src/main/java/it/unica/tcs/lib/model/Participant.java
@@ -1,15 +1,11 @@
 /*
- * Copyright 2017 Nicola Atzei
+ * Copyright 2018 Nicola Atzei
  */
 
 package it.unica.tcs.lib.model;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.InetAddress;
-import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -19,6 +15,8 @@ import java.util.function.Supplier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import it.unica.tcs.lib.model.ServerSocketDaemon.ServerSocketClient;
 
 public abstract class Participant implements Runnable {
 
@@ -57,6 +55,10 @@ public abstract class Participant implements Runnable {
         return receiverDeamon.getPort();
     }
 
+    public ServerSocketClient getClient() throws IOException {
+        return this.receiverDeamon.getClient();
+    }
+
     abstract public void run();
 
     public void parallel(Runnable... processes) {
@@ -89,27 +91,8 @@ public abstract class Participant implements Runnable {
     }
 
     public void send(String msg, Participant p) {
-
-        try (
-            Socket socket = new Socket(p.getHost(), p.getPort());
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter writer = new PrintWriter(socket.getOutputStream());
-        ) {
-            socket.setKeepAlive(true);
-            logger.trace("connected to "+socket.getInetAddress()+":"+socket.getPort()+", local port "+socket.getLocalPort());
-            logger.trace("isConnected "+socket.isConnected());
-            logger.trace("keepalive "+socket.getKeepAlive());
-            logger.trace("isBound "+socket.isBound());
-            logger.trace("isClosed "+socket.isClosed());
-            logger.trace("toString "+socket.toString());
-            logger.trace("sending "+msg);
-            writer.println(msg.trim());
-            writer.flush();
-            logger.trace("waiting the server to close the connection");
-            reader.readLine();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
+        try {
+            p.getClient().write(msg);
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
@@ -117,7 +100,6 @@ public abstract class Participant implements Runnable {
     }
 
     public String receive(Participant p) {
-        logger.trace("reading");
         try {
             return receiverDeamon.read();
         } catch (InterruptedException e) {
--- it.unica.tcs.bitcointm.lib/src/main/java/it/unica/tcs/lib/model/ServerSocketDaemon.java
+++ it.unica.tcs.bitcointm.lib/src/main/java/it/unica/tcs/lib/model/ServerSocketDaemon.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Nicola Atzei
+ * Copyright 2018 Nicola Atzei
  */
 
 package it.unica.tcs.lib.model;
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.InterruptedIOException;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.BlockingQueue;
@@ -19,14 +20,27 @@ import java.util.concurrent.TimeoutException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ServerSocketDaemon implements Runnable {
+public class ServerSocketDaemon  implements Runnable {
 
     private static final Logger logger = LoggerFactory.getLogger(ServerSocketDaemon.class);
 
     private BlockingQueue<String> buffer = new SynchronousQueue<>(true);
     private int port;
     private boolean online;
+    private Thread daemonThread;
 
+    /**
+     * Create a new daemon. The port is chosen randomly and will be set after the startup process.
+     * When {@link #isOnline()} returns true, then the port is set and cannot change during the daemon lifecycle.
+     */
+    public ServerSocketDaemon() {
+        this(0);
+    }
+
+    /**
+     * Create a new daemon a the specified port number.
+     * @param port the port that the daemon will use.
+     */
     public ServerSocketDaemon(int port) {
         this.port = port;
         this.online = false;
@@ -39,6 +53,14 @@ public class ServerSocketDaemon implements Runnable {
      */
     public int getPort() {
         return port;
+    }
+
+    /**
+     * Check if the server is running.
+     * @return true if the server started correctly, false otherwise.
+     */
+    public boolean isOnline() {
+        return online;
     }
 
     /**
@@ -58,6 +80,7 @@ public class ServerSocketDaemon implements Runnable {
      * @throws InterruptedException if the thread is interrupted
      */
     public String read() throws InterruptedException {
+        logger.trace("reading value");
         return buffer.take();
     }
 
@@ -78,10 +101,44 @@ public class ServerSocketDaemon implements Runnable {
         return value;
     }
 
-    public boolean isReady() {
-        return !buffer.isEmpty();
+    /**
+     * Start a new thread that executes the server.
+     */
+    public void start() {
+        logger.info("Starting...");
+        if (daemonThread != null)
+            throw new IllegalArgumentException("Daemon already started");
+
+        daemonThread = new Thread(this, "daemon");
+        daemonThread.start();
+        try {
+            waitUntilOnline();
+        } catch (InterruptedException e) {
+            logger.info("Interrupt signal!");
+            stop();
+            return;
+        }
+        logger.info("Server started");
     }
 
+    /**
+     * Stop the server through an interrupt signal.
+     */
+    public void stop() {
+        logger.info("Shutting down...");
+        daemonThread.interrupt();
+        logger.info("Stop");
+    }
+
+    /**
+     * Creates a new ServerSocket.
+     * Repeatedly wait for a new connection.
+     * <p>One connection allows to receive <b>just one string</b> at once.</p>
+     * <p>No multiple connections are allowed.</p>
+     * <p>The communication is synchronous. Once a connection is established,
+     * it waits indefinitely to receive a message. When a message is received,
+     * the server waits that someone read that value.</p>
+     */
     @Override
     public void run() {
 
@@ -92,7 +149,7 @@ public class ServerSocketDaemon implements Runnable {
             this.port = server.getLocalPort();  // if the port is 0, a free port is assigned by the system
             this.online = true;
 
-            logger.trace("daemon server started at port "+port);
+            logger.trace("server started at port "+port);
 
             while (true) {
 
@@ -101,21 +158,19 @@ public class ServerSocketDaemon implements Runnable {
                     return;
                 }
 
-                logger.trace("["+this.toString()+"] waiting for connection");
+                logger.trace("waiting for connection");
                 try (
                         Socket clientSocket = server.accept();
                         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                        PrintWriter out = new PrintWriter(clientSocket.getOutputStream())
                 ) {
-                    logger.trace("accepted connection from port <- "+clientSocket.getPort());
+                    logger.trace("new connection from {}:{} ", clientSocket.getInetAddress(), clientSocket.getPort());
                     logger.trace("waiting for input");
 
                     String inputLine = in.readLine();
-
-                    logger.trace("writing '"+inputLine+"' buffer");
+                    logger.trace("writing '"+inputLine+"' to buffer");
                     buffer.put(inputLine.toString());
 
-                    logger.trace("connection to port "+clientSocket.getPort()+" closed");
+                    logger.trace("<-/- "+clientSocket.getPort()+" : connection closed");
                 }
                 catch (InterruptedIOException e) {
                     logger.trace("Timeout...");
@@ -132,6 +187,50 @@ public class ServerSocketDaemon implements Runnable {
         } catch (IOException e) {
             logger.error("unable to start the server: "+e.getMessage());
             throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Return a Socket for this daemon. The socket is creating using
+     * {@code InetAddress.getLocalHost()} as host.
+     * @return a socket.
+     * @throws IOException if an I/O error occurs when creating the socket.
+     */
+    public Socket getSocket() throws IOException {
+        Socket socket = new Socket(InetAddress.getLocalHost(), getPort());
+        socket.setKeepAlive(true);
+        return socket;
+    }
+
+    /**
+     * Return a client that allows to write to this server.
+     * @return the client.
+     * @throws IOException if an I/O error occurs creating the socket.
+     */
+    public ServerSocketClient getClient() throws IOException {
+        return new ServerSocketClient(this);
+    }
+
+    public static class ServerSocketClient {
+
+        private static final Logger logger = LoggerFactory.getLogger(ServerSocketClient.class);
+        private final ServerSocketDaemon daemon;
+
+        private ServerSocketClient(ServerSocketDaemon daemon) throws IOException {
+            this.daemon = daemon;
+        }
+
+        public void write(String str) throws IOException {
+            Socket socket = daemon.getSocket();
+
+            logger.trace("connected to {}:{}, exit port {} ", socket.getInetAddress(), socket.getPort(), socket.getLocalPort());
+            try (
+                    PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    ) {
+                writer.println(str);
+                reader.readLine();      // wait until the server close the connection
+            }
         }
     }
 }
