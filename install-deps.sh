2cc4e7a04af9fd15426697d39b350e1a3db1d76d
Nicola Atzei <atzeinicola@gmail.com>
2018-03-19 17:35:02 +0100

    install-deps.sh: removed symlink creation

--- install-deps.sh
+++ install-deps.sh
@@ -8,7 +8,3 @@ git -C $HOME clone https://github.com/natzei/bitcoinj.git
 git -C $HOME/bitcoinj checkout lib
 mvn -f $HOME/bitcoinj install -DskipTests
 rm -rf $HOME/bitcoinj
-
-echo "Symlink it.unica.tcs.bitcointm/it.unica.tcs.bitcointm.lib-0.0.1-bundled.jar -> ../it.unica.tcs.bitcointm.lib/target/it.unica.tcs.bitcointm.lib-0.0.1-bundled.jar"
-ln -t it.unica.tcs.bitcointm -s ../it.unica.tcs.bitcointm.lib/target/it.unica.tcs.bitcointm.lib-0.0.1-bundled.jar
-
