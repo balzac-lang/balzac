2bad6587aab3d139009583bad4190978fd62115f
Nicola Atzei <atzeinicola@gmail.com>
2018-03-22 09:56:08 +0100

    Update install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -1,5 +1,9 @@
+rm -rf $HOME/BitcoindConnector4J
+rm -rf $HOME/bitcoinj
+
 echo "Cloning https://github.com/natzei/BitcoindConnector4J.git"
 git -C $HOME clone https://github.com/natzei/BitcoindConnector4J.git
+git -C $HOME/BitcoindConnector4J checkout release-0.16
 gradle -p $HOME/BitcoindConnector4J install
 rm -rf $HOME/BitcoindConnector4J
 
