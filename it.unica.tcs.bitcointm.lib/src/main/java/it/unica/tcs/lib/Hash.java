e9c8e4f09a73e94a2034a736e29b0161a76e0554
Nicola Atzei <atzeinicola@gmail.com>
2018-12-23 19:57:18 +0100

    Lib: Hash implements Comparable

--- it.unica.tcs.bitcointm.lib/src/main/java/it/unica/tcs/lib/Hash.java
+++ it.unica.tcs.bitcointm.lib/src/main/java/it/unica/tcs/lib/Hash.java
@@ -1,13 +1,14 @@
 /*
- * Copyright 2017 Nicola Atzei
+ * Copyright 2018 Nicola Atzei
  */
 package it.unica.tcs.lib;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 
 import it.unica.tcs.lib.utils.BitcoinUtils;
 
-public class Hash {
+public class Hash implements Comparable<Hash> {
 
     private final byte[] bytes;
     public enum HashAlgorithm { SHA256, RIPEMD160, HASH256, HASH160, SHA1 }
@@ -22,6 +23,10 @@ public class Hash {
 
     public String getBytesAsString() {
         return BitcoinUtils.encode(bytes);
+    }
+
+    public static Hash fromString(String str) {
+        return new Hash(BitcoinUtils.decode(str));
     }
 
     @Override
@@ -49,5 +54,15 @@ public class Hash {
     @Override
     public String toString() {
         return getBytesAsString();
+    }
+
+    @Override
+    public int compareTo(Hash o) {
+        if (bytes.length != o.bytes.length) {
+            return bytes.length - o.bytes.length;
+        }
+        BigInteger a = new BigInteger(1, bytes);
+        BigInteger b = new BigInteger(1, o.bytes);
+        return a.compareTo(b);
     }
 }
---
+++ it.unica.tcs.bitcointm.lib/src/test/java/it/unica/tcs/lib/TestHash.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2018 Nicola Atzei
+ */
+package it.unica.tcs.lib;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestHash {
+
+    @Test
+    public void testCompareTo() {
+        Hash a = Hash.fromString("000000");
+        Hash b = Hash.fromString("000123");        
+        assertTrue(a.compareTo(b) < 0);
+        assertTrue(b.compareTo(a) > 0);
+    }
+
+    @Test
+    public void testCompareToEqualsSameLength() {
+        Hash a = Hash.fromString("00000123");
+        Hash b = Hash.fromString("00000123");        
+        assertTrue(a.compareTo(b) == 0);
+        assertTrue(b.compareTo(a) == 0);
+        assertTrue(a.equals(b));
+        assertTrue(b.equals(a));
+    }
+
+    @Test
+    public void testCompareToEqualsOfDifferentLength() {
+        Hash a = Hash.fromString("000123");
+        Hash b = Hash.fromString("00000123");        
+        assertTrue(a.compareTo(b) < 0);
+        assertTrue(b.compareTo(a) > 0);
+        assertFalse(a.equals(b));
+        assertFalse(b.equals(a));
+    }
+}
