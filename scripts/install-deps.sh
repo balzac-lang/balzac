d075a9b132c257836f1e7ae93e7302d8b7b8a71e
Nicola Atzei <atzeinicola@gmail.com>
2019-05-19 16:44:58 +0200

    Update bitcoinj version

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -4,11 +4,11 @@ rm -rf $HOME/bitcoinj
 echo "Cloning https://github.com/natzei/BitcoindConnector4J.git"
 git -C $HOME clone https://github.com/natzei/BitcoindConnector4J.git
 git -C $HOME/BitcoindConnector4J checkout v0.16.0
-gradle -p $HOME/BitcoindConnector4J install
+./gradlew -p $HOME/BitcoindConnector4J install
 rm -rf $HOME/BitcoindConnector4J
 
 echo "Cloning https://github.com/natzei/bitcoinj.git"
 git -C $HOME clone https://github.com/natzei/bitcoinj.git
-git -C $HOME/bitcoinj checkout lib
-gradle -p $HOME/bitcoinj install -x test
+git -C $HOME/bitcoinj checkout 0.16.1-LIB
+./gradlew -p $HOME/bitcoinj install -x test
 rm -rf $HOME/bitcoinj
