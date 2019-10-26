d567d124eeff3da71bc7ac0ef467f3cb1176adf5
Nicola Atzei <atzeinicola@gmail.com>
2019-10-26 08:48:31 +0200

    Update Copyright header

--- scripts/update-copyright.sh
+++ scripts/update-copyright.sh
@@ -8,13 +8,34 @@ for FILE in $REPO_FILES ; do
 
     echo "$MODIFICATION_YEAR $MODIFICATION_DATE $FILE"
 
+COPYRIGHT_NOTES=$(cat <<-END
+/*
+ * Copyright $MODIFICATION_YEAR Nicola Atzei
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+END
+)
+
+    echo -e "${COPYRIGHT_NOTES}"
+
     if [[ $(cat $FILE | grep "Copyright") == "" ]]; then
         # File has no copyright string
         echo -e "\tAdding new copyright string"
-        NEW_COPYRIGHT_STRING=`echo -e "/*\n * Copyright $MODIFICATION_YEAR Nicola Atzei\n */"`
-        echo -e "$NEW_COPYRIGHT_STRING\n$(cat $FILE)" > $FILE
+        echo -e "${COPYRIGHT_NOTES}\n$(cat $FILE)" > $FILE
     else
         echo -e "\tUpdating existing copyright string"
+        #echo -e "${COPYRIGHT_NOTES}\n$(cat $FILE | awk '{ if ( NR > 3  ) { print } }')" > $FILE
         OLD_COPYRIGHT_STRING="Copyright [0-9]{4} Nicola Atzei"
         NEW_COPYRIGHT_STRING="Copyright $MODIFICATION_YEAR Nicola Atzei"
         sed -i -r "s/$OLD_COPYRIGHT_STRING/$NEW_COPYRIGHT_STRING/g" $FILE
