c33910fdd8955b13fd165f23946d16d1ca7ae568
Nicola Atzei <atzeinicola@gmail.com>
2019-11-03 11:17:20 +0100

    Update Travis CI

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -2,6 +2,12 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
+echo "Pushing with tag 'latest'"
 docker push balzaclang/balzac:latest
 docker push balzaclang/balzac-arm32v7:latest
 docker push balzaclang/balzac-arm64v8:latest
+
+echo "Pushing with tag ${LATEST_VERSION}"
+docker push balzaclang/balzac:${LATEST_VERSION}
+docker push balzaclang/balzac-arm32v7:${LATEST_VERSION}
+docker push balzaclang/balzac-arm64v8:${LATEST_VERSION}
