ee424c2f1c5c891303dd9e11826abd1ecc713bfe
Nicola Atzei <atzeinicola@gmail.com>
2022-09-15 12:48:48 +0200

    Remove Travis CI

--- scripts/docker_push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Login in docker..."
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-
-echo "Pushing with tag ${TAG_VERSION}"
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t balzaclang/balzac:${TAG_VERSION} --push .
--- scripts/install-deps.sh
+++
@@ -1,9 +0,0 @@
-[[ "${BITCOINJ_TAG}" == "" ]] && (echo "BITCOINJ_TAG is not set" && exit 1)
-
-rm -rf $HOME/bitcoinj || (echo "Unable to delete dir $HOME/bitcoinj" && exit 1)
-
-echo "Cloning https://github.com/natzei/bitcoinj.git"
-git -C $HOME clone https://github.com/natzei/bitcoinj.git || (echo "Unable to clone https://github.com/natzei/bitcoinj.git" && exit 1)
-git -C $HOME/bitcoinj checkout ${BITCOINJ_TAG} || (echo "Unable to checkout branch ${BITCOINJ_TAG}" && exit 1)
-$HOME/bitcoinj/gradlew -p $HOME/bitcoinj publishToMavenLocal -x test || (echo "Unable to gradle install bitcoinj" && exit 1)
-rm -rf $HOME/bitcoinj || (echo "Unable to delete dir $HOME/bitcoinj" && exit 1)
